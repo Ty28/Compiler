03abc9c2e3b236cb90a0640122adbabf4abdcceb
encorew <encore.w@qq.com>
2021-04-13 04:49:54 -0700

    insertTuple bug in symbol.c

--- Lab1/Lab/Code/symbol.c
+++ Lab1/Lab/Code/symbol.c
@@ -12,51 +12,60 @@ unsigned int hashProject(char *name)
     return val;
 }
 
-Symbol* createSymbolTable()//the elements of our Hashtable are pointers
+Symbol *createSymbolTable() //the elements of our Hashtable are pointers
 {
-    Symbol* tableHead = (Symbol*)malloc(HASHSIZE * sizeof(Symbol*));
+    Symbol *tableHead = (Symbol *)malloc(HASHSIZE * sizeof(Symbol *));
     for (int i = 0; i < HASHSIZE; i++) //initialize
     {
-        tableHead[i]=NULL;
+        tableHead[i] = NULL;
     }
     return tableHead;
 }
 
 Type createBasicType(int _basic)
 {
-    Type tupleType=(Type)malloc(sizeof(struct Type_));
-    tupleType->kind=BASIC;
-    tupleType->u.basic=_basic;
+    Type tupleType = (Type)malloc(sizeof(struct Type_));
+    tupleType->kind = BASIC;
+    tupleType->u.basic = _basic;
     return tupleType;
 }
 
-Type createArrayType(Type _elem,int _size)
+Type createArrayType(Type _elem, int _size)
 {
-    Type tupleType=(Type)malloc(sizeof(struct Type_));
-    tupleType->kind=ARRAY;
-    tupleType->u.array.elem=_elem;
-    tupleType->u.array.size=_size;
+    Type tupleType = (Type)malloc(sizeof(struct Type_));
+    tupleType->kind = ARRAY;
+    tupleType->u.array.elem = _elem;
+    tupleType->u.array.size = _size;
     return tupleType;
 }
 
 Type createStructType(Symbol _structure)
 {
-    Type tupleType=(Type)malloc(sizeof(struct Type_));
-    tupleType->kind=STRUCTURE;
-    tupleType->u.structure=_structure;
+    Type tupleType = (Type)malloc(sizeof(struct Type_));
+    tupleType->kind = STRUCTURE;
+    tupleType->u.structure = _structure;
     return tupleType;
 }
 
-Type createType(int _kind,int _basic,Type _elem,int _size,Symbol _structure)
+Type createFuncType(int _funcBasic, Symbol _parameter)
 {
-    switch(_kind)
+    Type tupleType = (Type)malloc(sizeof(struct Type_));
+    tupleType->kind = FUNCTION;
+    tupleType->u.function.funcBasic = _funcBasic;
+    tupleType->u.function.parameter = _parameter;
+    return tupleType;
+}
+
+Type createType(int _kind, int _basic, Type _elem, int _size, Symbol _structure)
+{
+    switch (_kind)
     {
-    case(1):
+    case (1):
         return createBasicType(_basic);
-    case(2):
-        return createArrayType(_elem,_size);
-    case(3):
-        return createStructType(_structure);   
+    case (2):
+        return createArrayType(_elem, _size);
+    case (3):
+        return createStructType(_structure);
     }
 }
 
@@ -67,49 +76,49 @@ Symbol createBlankTuple(char *name)
     newTuple->hashLink = NULL;
     newTuple->structLink = NULL;
     return newTuple;
-
 }
 
-Symbol createBasicTuple(char *name,int _basic)
+Symbol createBasicTuple(char *name, int _basic)
 {
-    Type tupleType=(Type)malloc(sizeof(struct Type_));
-    tupleType->kind=BASIC;
-    tupleType->u.basic=_basic;
-    Symbol newTuple=createBlankTuple(name);
-    newTuple->type=tupleType;
+    Type tupleType = createBasicType(_basic);
+    Symbol newTuple = createBlankTuple(name);
+    newTuple->type = tupleType;
     return newTuple;
 }
 
-Symbol createArrayTuple(char *name,Type _elem,int _size)
+Symbol createArrayTuple(char *name, Type _elem, int _size)
 {
-    Type tupleType=(Type)malloc(sizeof(struct Type_));
-    tupleType->kind=ARRAY;
-    tupleType->u.array.elem=_elem;
-    tupleType->u.array.size=_size;
-    Symbol newTuple=createBlankTuple(name);
-    newTuple->type=tupleType;
+    Type tupleType = createArrayType(_elem, _size);
+    Symbol newTuple = createBlankTuple(name);
+    newTuple->type = tupleType;
     return newTuple;
 }
 
-Symbol createStructTuple(char *name,Symbol structNext)
+Symbol createStructTuple(char *name, Symbol _structure)
 {
-    Type tupleType=(Type)malloc(sizeof(struct Type_));
-    tupleType->kind=STRUCTURE;
-    tupleType->u.structure=structNext;
-    Symbol newTuple=createBlankTuple(name);
-    newTuple->type=tupleType;
+    Type tupleType = createStructType(_structure);
+    Symbol newTuple = createBlankTuple(name);
+    newTuple->type = tupleType;
+    return newTuple;
+}
+
+Symbol createFuncTuple(char *name, int _funcBasic, Symbol _parameter)
+{
+    Type tupleType = createFuncType(_funcBasic, _parameter);
+    Symbol newTuple = createBlankTuple(name);
+    newTuple->type = tupleType;
     return newTuple;
 }
 
 Symbol findSymbol(char *name)
 {
     unsigned int hashID = hashProject(name);
-    if (symbolTable[hashID]!=NULL)
+    if (symbolTable[hashID] != NULL)
     {
         Symbol current = symbolTable[hashID];
         while (current != NULL)
         {
-            if (strcmp(current->name, name) == 0)//loop until we find that name
+            if (strcmp(current->name, name) == 0) //loop until we find that name
             {
                 return current;
             }
@@ -125,13 +134,13 @@ void insertTuple(Symbol tuple)
     if (find != NULL)
         return;
     unsigned int hashID = hashProject(tuple->name);
-    Symbol current = symbolTable[hashID];
-    if (current==NULL)
+    if (symbolTable[hashID] == NULL)
     {
-        current=tuple;//make the NULL pointer pointed to tuple
+        symbolTable[hashID] = tuple; //make the NULL pointer pointed to the tuple
         return;
-    }    
+    }
+    Symbol current = symbolTable[hashID]->hashLink;
     while (current->hashLink != NULL)
         current = current->hashLink;
-    current->hashLink=tuple;
+    current->hashLink = tuple;
 }
