39856971a68c698567b5abf803b0dbe880a17f69
encorew <encore.w@qq.com>
2021-04-13 05:59:24 -0700

    refresh

--- Lab1/Lab/Code/main.c
+++ Lab1/Lab/Code/main.c
@@ -13,10 +13,6 @@ int main(int argc, char** argv) {
     if(lexicalError == 0 && syntaxError == 0)
         preOrder(ROOT, 0);
     else 
-<<<<<<< HEAD:Lab1/Lab/Code/main.c
-        printf("\n");
-=======
         printf("%d  %d", lexicalError, syntaxError);
->>>>>>> 940517cf349ca7537fb2841f6d316531f54eb4e8:Lab/Code/main.c
     return 0;
 }
