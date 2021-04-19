20f57e1bd19a2bd4290167522524f4a77639921b
encorew <encore.w@qq.com>
2021-04-18 19:58:25 -0700

    commit before change branch

--- Lab2/Lab/Code/main.c
+++ Lab2/Lab/Code/main.c
@@ -1,8 +1,50 @@
 #include "extern.h"
 
-int main(int argc, char** argv) {
-    if (argc <= 1) return 1;
-    FILE* f = fopen(argv[1], "r");
+void printStructure(FieldList field)
+{
+    while (field != NULL)
+    {
+        printf("%d : %s", field->type->kind, field->name);
+        if (field->type->kind == ARRAY)
+        {
+            Type myType = field->type;
+            while (1)
+            {
+                if (myType->kind == ARRAY)
+                {
+                    printf(" %d ", myType->u.array.size);
+                    myType = myType->u.array.elem;
+                }
+                else if (myType->kind == BASIC)
+                {
+                    printf(" BASIC:%d", myType->u.basic);
+                    break;
+                }
+                else if (myType->kind == STRUCTVAR)
+                {
+                    printf(" STRUCTVAR");
+                    printf("\n\n-------------\n");
+                    printStructure(myType->u.structure);
+                    printf("\n-------------\n\n");
+                    break;
+                }
+                else if (myType->kind == STRUCTURE)
+                {
+                    printf(" STRUCTURE");
+                    break;
+                }
+            }
+        }
+        printf("\n*\n");
+        field = field->tail;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc <= 1)
+        return 1;
+    FILE *f = fopen(argv[1], "r");
     if (!f)
     {
         perror(argv[1]);
@@ -10,11 +52,14 @@ int main(int argc, char** argv) {
     }
     yyrestart(f);
     yyparse();
-    if(lexicalError == 0 && syntaxError == 0){
-        preOrder(ROOT, 0);
+    if (lexicalError == 0 && syntaxError == 0)
+    {
+        //preOrder(ROOT, 0);
         semanticCheck(ROOT);
+        // Symbol p = findSymbol("k3");
+        // //printf("%s",p);
+        // FieldList field = p->type->u.structure;
+        // printStructure(field);
     }
-    // else 
-    //     printf("\n");
     return 0;
 }
