dc9c55e07e22fdd7e9acf0c7c8488e1b867293da
encorew <encore.w@qq.com>
2021-05-15 01:38:15 -0700

    change output filename

--- Lab3/Lab/Code/interCodePrint.c
+++ Lab3/Lab/Code/interCodePrint.c
@@ -29,10 +29,13 @@ void printOpSingle(FILE *fp, Operand op)
     }
 }
 
-void printCode()
+void printCode(char *outputFileName)
 {
     FILE *fp;
-    fp = fopen("out.ir", "w+");
+    if (strcmp(outputFileName, "\0") == 0)
+        fp = fopen("out.ir", "w+");
+    else
+        fp = fopen(outputFileName, "w+");
     InterCode current = head;
     while (current != NULL)
     {
@@ -40,93 +43,93 @@ void printCode()
         {
         case (MYFUNCTION):
             fprintf(fp, "FUNCTION ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             fprintf(fp, " :");
             break;
         case (MYPARAM):
             fprintf(fp, "PARAM ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             break;
         case (MYRETURN):
             fprintf(fp, "RETURN ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             fprintf(fp, "\n");
             break;
         case (MYLABEL):
             fprintf(fp, "LABEL ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             fprintf(fp, " :");
             break;
         case (MYGOTO):
             fprintf(fp, "GOTO ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             break;
         //TODO:need to complete MYREAD and MYWRITE
         case (MYREAD):
             fprintf(fp, "READ ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             break;
         case (MYWRITE):
             fprintf(fp, "WRITE ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             break;
         //TODO:need to complete MYARG
         case (MYARG):
             fprintf(fp, "ARG ");
-            printOpSingle(fp,current->u.op_single.op);
+            printOpSingle(fp, current->u.op_single.op);
             break;
         case (MYASSIGN):
-            printOpSingle(fp,current->u.op_assign.left);
+            printOpSingle(fp, current->u.op_assign.left);
             fprintf(fp, " := ");
-            printOpSingle(fp,current->u.op_assign.right);
+            printOpSingle(fp, current->u.op_assign.right);
             break;
         case (MYDEC):
             fprintf(fp, "DEC ");
-            printOpSingle(fp,current->u.op_dec.op);
+            printOpSingle(fp, current->u.op_dec.op);
             fprintf(fp, " %d", current->u.op_dec.size);
             break;
         //TODO:need to complete MYCALL
         case (MYCALL):
-            printOpSingle(fp,current->u.op_assign.left);
+            printOpSingle(fp, current->u.op_assign.left);
             fprintf(fp, " := CALL ");
-            printOpSingle(fp,current->u.op_assign.right);
+            printOpSingle(fp, current->u.op_assign.right);
             break;
         case (MYADD):
-            printOpSingle(fp,current->u.op_binary.result);
+            printOpSingle(fp, current->u.op_binary.result);
             fprintf(fp, " := ");
-            printOpSingle(fp,current->u.op_binary.op1);
+            printOpSingle(fp, current->u.op_binary.op1);
             fprintf(fp, " + ");
-            printOpSingle(fp,current->u.op_binary.op2);
+            printOpSingle(fp, current->u.op_binary.op2);
             break;
         case (MYSUB):
-            printOpSingle(fp,current->u.op_binary.result);
+            printOpSingle(fp, current->u.op_binary.result);
             fprintf(fp, " := ");
-            printOpSingle(fp,current->u.op_binary.op1);
+            printOpSingle(fp, current->u.op_binary.op1);
             fprintf(fp, " - ");
-            printOpSingle(fp,current->u.op_binary.op2);
+            printOpSingle(fp, current->u.op_binary.op2);
             break;
         case (MYMUL):
-            printOpSingle(fp,current->u.op_binary.result);
+            printOpSingle(fp, current->u.op_binary.result);
             fprintf(fp, " := ");
-            printOpSingle(fp,current->u.op_binary.op1);
+            printOpSingle(fp, current->u.op_binary.op1);
             fprintf(fp, " * ");
-            printOpSingle(fp,current->u.op_binary.op2);
+            printOpSingle(fp, current->u.op_binary.op2);
             break;
         case (MYDIV):
-            printOpSingle(fp,current->u.op_binary.result);
+            printOpSingle(fp, current->u.op_binary.result);
             fprintf(fp, " := ");
-            printOpSingle(fp,current->u.op_binary.op1);
+            printOpSingle(fp, current->u.op_binary.op1);
             fprintf(fp, " / ");
-            printOpSingle(fp,current->u.op_binary.op2);
+            printOpSingle(fp, current->u.op_binary.op2);
             break;
             break;
         case (MYIFGOTO):
             fprintf(fp, "IF ");
-            printOpSingle(fp,current->u.op_triple.x);
+            printOpSingle(fp, current->u.op_triple.x);
             fprintf(fp, " %s ", current->u.op_triple.relop);
-            printOpSingle(fp,current->u.op_triple.y);
+            printOpSingle(fp, current->u.op_triple.y);
             fprintf(fp, " GOTO ");
-            printOpSingle(fp,current->u.op_triple.label);
+            printOpSingle(fp, current->u.op_triple.label);
             break;
         //TODO:need to complete MYADDRESS ?
         case (MYADDRESS):
--- Lab3/Lab/Code/interCodePrint.h
+++ Lab3/Lab/Code/interCodePrint.h
@@ -2,8 +2,5 @@
 #define __INTERCODEPRINT_H
 #include "interCode.h"
 void printOpSingle(FILE *fp, Operand op);
-void printOpAssign(Operand left, Operand right);
-void printOpBinary(Operand result, Operand left, Operand right);
-void printOpTriple(Operand x, Operand y, Operand laebl, char *relop);
-void printCode();
+void printCode(char *outputFileName);
 #endif
